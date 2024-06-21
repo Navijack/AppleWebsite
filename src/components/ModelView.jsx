ab98b7ff6bcf66b1e3ef313e5488045b18eb0014
Johnny Costa <Johnny_nike@live.com.pt>
2024-06-22 00:28:09 +0200

    3d iphone

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,26 @@
+import { PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights";
+import { Suspense } from "react";
 
-const ModelView = () => {
+
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights>
+      </Lights>
+      <Suspense fallback={<html><div>Loading</div></html>}>
+      </Suspense>
+
+    </View >
   );
 };
 
