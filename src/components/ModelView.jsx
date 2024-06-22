27a19b2f8c758fd0f344995f2fdb909353344942
Johnny Costa <Johnny_nike@live.com.pt>
2024-06-22 15:56:51 +0200

    Features

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,26 +1,29 @@
-import { PerspectiveCamera, View } from "@react-three/drei"
+import { PerspectiveCamera, View } from "@react-three/drei";
 import Lights from "./Lights";
 import { Suspense } from "react";
 
-
-
-const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
+const ModelView = ({ index, gsapType }) => {
   return (
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+      className={`border-2 border-red-500 w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
 
+      {/* Perspective Camera */}
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
-      <Lights>
-      </Lights>
-      <Suspense fallback={<html><div>Loading</div></html>}>
+
+      {/* Lights */}
+      <Lights />
+
+      {/* Suspense Fallback */}
+      <Suspense fallback={<div>Loading...</div>}>
       </Suspense>
 
-    </View >
+
+    </View>
   );
 };
 
