f8c93f5251d98e1f6aab298712c0131998fb5731
Johnny Costa <Johnny_nike@live.com.pt>
2024-06-24 17:19:44 +0200

    bug

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,12 @@
-import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
-import Lights from "./Lights"
-import { Suspense } from "react"
-import IPhone from "./IPhone";
-import * as THREE from "three";
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 
-const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
+import * as THREE from 'three'
+import Lights from './Lights';
+import Loader from "./Loader";
+import IPhone from './IPhone';
+import { Suspense } from "react";
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
     <View
       index={index}
@@ -25,9 +27,9 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
         onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
       />
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0, 0]}>
-        <Suspense fallback={<div>Loading</div>}>
+        <Suspense fallback={<Loader />}>
           <IPhone
-            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            scale={index === 10 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
             size={size}
           />
