5c71cd315f3f8a61f21d20ddae7baf1fd49b72bd
Johnny Costa <Johnny_nike@live.com.pt>
2024-06-24 16:35:59 +0200

    Final step without errors

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,21 @@
+import { PerspectiveCamera, View } from "@react-three/drei"
+import Lights from "./Lights"
+import { Suspense } from "react"
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
   return (
-    <div>ModelView</div>
-  );
-};
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full border-red-500 h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}>
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
-export default ModelView;
+      <Lights />
+      <Suspense fallback={<div>Loading</div>}>
+      </Suspense>
+    </View>
+  )
+}
+export default ModelView 
