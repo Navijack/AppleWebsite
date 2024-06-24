17af40dc62614afa16c07b0a00b44e67b2d067b2
Johnny Costa <Johnny_nike@live.com.pt>
2024-06-24 16:16:49 +0200

    model updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer"
 
 const App = () => {
 
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,29 +1,7 @@
-import { PerspectiveCamera, View } from "@react-three/drei";
-import Lights from "./Lights";
-import { Suspense } from "react";
 
-const ModelView = ({ index, gsapType }) => {
+const ModelView = () => {
   return (
-    <View
-      index={index}
-      id={gsapType}
-      className={`border-2 border-red-500 w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
-    >
-      {/* Ambient Light */}
-      <ambientLight intensity={0.3} />
-
-      {/* Perspective Camera */}
-      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
-
-      {/* Lights */}
-      <Lights />
-
-      {/* Suspense Fallback */}
-      <Suspense fallback={<div>Loading...</div>}>
-      </Suspense>
-
-
-    </View>
+    <div>ModelView</div>
   );
 };
 
